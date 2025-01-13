50a8d6537df62d519ab52ca20db4e971bc9b0077
renan <renav.guedes7@gmail.com>
2025-01-12 22:53:01 -0300

    fix: change backend port

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(express.json());
