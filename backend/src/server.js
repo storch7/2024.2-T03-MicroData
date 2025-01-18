1897cb3847b8bdbb751213b14690af09ebec5c34
Breno Soares Fernandes <breno.fernandes@serpro.gov.br>
2025-01-18 14:26:29 -0300

    atualizando backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ const app = require('./app');
 // Define a porta em que o servidor irá rodar.
 // Primeiro verifica se a variável de ambiente `PORT` está definida (útil em produção, como em serviços na nuvem).
 // Caso contrário, utiliza a porta padrão `3000`.
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Inicia o servidor, fazendo com que ele escute as requisições na porta definida.
 // O método `listen` aceita dois parâmetros: a porta e uma função de callback que será executada assim que o servidor estiver rodando.
