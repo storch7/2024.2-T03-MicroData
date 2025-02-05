facf306f1ad68c7f894140237dc2c5c977cec9b0
storch7 <guilhermestorch7@gmail.com>
2025-02-04 23:51:11 -0300

    Textfield limite contagem

--- frontend/src/components/LimiteContagemForm.jsx
+++ frontend/src/components/LimiteContagemForm.jsx
@@ -1,11 +1,23 @@
+import React, {useEffect, useState} from "react";
 import CustomButton from '../components/Button';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 
 function LimiteContagemForm() {
+    const [LimiteContagem, setLimiteContagem] = useState("");
+
     return(
         <div>
-            ESSE É O FORMULÁRIO DE CADASTRO DE LIMITE DE CONTAGEM
+            <Box component="form">
+                
+                <TextField
+                    label="Limite de Contagem" 
+                    value={LimiteContagem} 
+                    onChange={(e) => setLimiteContagem(e.target.value)} 
+                    fullWidth
+                />
+                
+            </Box>
         </div>
     );
 };
