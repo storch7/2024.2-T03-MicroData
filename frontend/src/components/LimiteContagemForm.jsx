c5ab51b1c0cbb095e30fc202b3097ece6a307d6b
storch7 <guilhermestorch7@gmail.com>
2025-02-04 23:46:28 -0300

    Criação do formulário de cadastro do limite de contagem

---
+++ frontend/src/components/LimiteContagemForm.jsx
@@ -0,0 +1,13 @@
+import CustomButton from '../components/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+
+function LimiteContagemForm() {
+    return(
+        <div>
+            ESSE É O FORMULÁRIO DE CADASTRO DE LIMITE DE CONTAGEM
+        </div>
+    );
+};
+
+export default LimiteContagemForm;
--- frontend/src/pages/LimiteContagem.jsx
+++ frontend/src/pages/LimiteContagem.jsx
@@ -1,8 +1,19 @@
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import LimiteContagemForm from '../components/LimiteContagemForm';
+
+
 function LimiteContagem () {
     return(
         <section>
+            <Container>
+                <Typography variant="h4" component="h1" gutterBottom>
+                    Novo Limite de Contagem
+                </Typography>
 
-            
+                <LimiteContagemForm />
+            </Container>
+
         </section>
     )
 }
