6806f458c5229f0a4fc090bf3e02a06062f64fab
Renan Vieira <renanv.guedes7@gmail.com>
2025-02-08 17:25:42 -0300

    fix: remove all migrations

--- backend/prisma/migrations/20250118171748_test/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `resultado_coleta` on the `Resultados` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Resultados" DROP COLUMN "resultado_coleta";
--- backend/prisma/migrations/20250206183018_init/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "LimitesContagem" ADD COLUMN     "ativo" BOOLEAN NOT NULL DEFAULT true,
-ADD COLUMN     "data_desativacao" TIMESTAMP(3);
