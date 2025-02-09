bc7ef63bf36de23e0ff91e3faa24ab758a40b81b
Renan Vieira <renanv.guedes7@gmail.com>
2025-02-09 01:42:43 -0300

    feat: add InputSelect component

---
+++ frontend/src/components/InputSelect.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+
+export default function InputSelect({ label, items, value, onChange, helperText }) {
+  return (
+    <TextField
+      select
+      label={label}
+      value={value}
+      onChange={onChange}
+      margin="normal"
+      variant="outlined"
+      fullWidth
+    >
+      {items && items.length > 0 ? (
+        items.map((item) => (
+          <MenuItem key={item.id} value={item.id}>
+            {item.nome}
+          </MenuItem>
+        ))
+      ) : (
+        <MenuItem disabled>No options available</MenuItem>
+      )}
+    </TextField>
+  );
+}
