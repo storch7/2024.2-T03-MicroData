161d7a4ff13287043b543995ada6e3210245a22e
Breno Soares Fernandes <breno.fernandes@serpro.gov.br>
2025-02-09 17:34:26 -0300

    Adicionando funções e rotas do backend de resultados

--- backend/src/routes/limitesContagemRoutes.js
+++ backend/src/routes/limitesContagemRoutes.js
@@ -2,14 +2,12 @@
 const express = require('express');
 
 // Importa a função `createMicroorganismo` do arquivo de controlador localizado no diretório `controllers`.
-// Essa função é responsável por lidar com a lógica de criação de um microorganismo.
 const { createLimiteContagem, getLimiteContagem, updateLimiteContagem } = require('../controllers/limitesContagemController');
 
 // Cria uma nova instância de um roteador do Express, que será usada para definir as rotas específicas deste recurso.
 const router = express.Router();
 
 // Define uma rota HTTP do tipo POST na raiz (`/`) deste roteador.
-// Quando essa rota é acessada, ela chama a função `createMicroorganismo` para processar a requisição.
 router.post('/', createLimiteContagem);
 router.get('/', getLimiteContagem);
 router.put('/:id', updateLimiteContagem); 
---
+++ backend/src/routes/resultadosRoutes.js
@@ -0,0 +1,18 @@
+// Importa o módulo `express`, que é o framework usado para criar servidores e rotas HTTP.
+const express = require('express');
+
+// Importa a função `createMicroorganismo` do arquivo de controlador localizado no diretório `controllers`.
+const { createResultado, getResultado, updateResultado } = require('../controllers/resultadosController');
+
+// Cria uma nova instância de um roteador do Express, que será usada para definir as rotas específicas deste recurso.
+const router = express.Router();
+
+// Define uma rota HTTP do tipo POST na raiz (`/`) deste roteador.
+router.post('/', createResultado);
+router.get('/', getResultado);
+router.put('/:id', updateResultado); 
+
+
+
+// Exporta o roteador para que ele possa ser usado em outros arquivos (como no `app.js`).
+module.exports = router;
