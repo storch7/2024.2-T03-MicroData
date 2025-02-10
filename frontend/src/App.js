4961b3c487f8623082065ac1bcc8f0dd70b3b7c2
storch7 <guilhermestorch7@gmail.com>
2025-02-10 00:31:09 -0300

    COrreção da renderização da página do dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import MicroorganismPage from './pages/Microorganism';
 import PontosavaliadosPage from './pages/Pontosavaliados';
 import LimiteContagemPage from './pages/LimiteContagem';
+import Dashboard from './pages/Dash/Dashboard';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         <Route path="/microorganism" element={<MicroorganismPage />} />
         <Route path="/pontosavaliados" element={<PontosavaliadosPage />} />
         <Route path="/limites-contagem" element={<LimiteContagemPage />} />
-        <Route path="/" element={<MicroorganismPage />} /> {/* Rota padrão */}
+        <Route path="/" element={<Dashboard />} /> {/* Rota padrão */}
       </Routes>
     </div>
   );
---
+++ frontend/src/pages/Dash/Dashboard.jsx
@@ -0,0 +1,9 @@
+import './index.css'
+
+export default function Dashboard() {
+    return(
+        <section>
+            <h1>DASHBOARD</h1>
+        </section>
+    )
+}
