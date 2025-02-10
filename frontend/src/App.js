800481b8f62cdb14105170f6c16c82f2e8321f12
storch7 <guilhermestorch7@gmail.com>
2025-02-10 09:33:31 -0300

    Configuração da rota e renderização do componente da página

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import MicroorganismPage from './pages/Microorganism';
 import PontosavaliadosPage from './pages/Pontosavaliados';
 import LimiteContagemPage from './pages/LimiteContagem';
+import Resultados from './pages/Resultados'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/microorganism" element={<MicroorganismPage />} />
         <Route path="/pontosavaliados" element={<PontosavaliadosPage />} />
         <Route path="/limites-contagem" element={<LimiteContagemPage />} />
+        <Route path="/resultados-coleta" element={<Resultados />} />
         <Route path="/" element={<MicroorganismPage />} /> {/* Rota padrão */}
       </Routes>
     </div>
---
+++ frontend/src/pages/Resultados.jsx
@@ -0,0 +1,8 @@
+export default function Resultados () {
+    
+    return(
+        <section>
+        <h1>pagina de resultados</h1>
+        </section>
+    )
+}
